425304a987d6b3ebcb11e19082589fc5f330368d
Amar Prasad Rath <ar16@iitbbs.ac.in>
2016-10-30 01:26:27 +0530

    Initial commit, started project

---
+++ src/read_video.cpp
@@ -0,0 +1,10 @@
+/*
+ * read_video.cpp
+ *
+ *  Created on: 30-Oct-2016
+ *      Author: amar
+ */
+
+
+
+
---
+++ src/read_video.h
@@ -0,0 +1,24 @@
+/*
+ * read_video.h
+ *
+ *  Created on: 30-Oct-2016
+ *      Author: amar
+ */
+
+#ifndef SRC_READ_VIDEO_H_
+#define SRC_READ_VIDEO_H_
+
+#include "opencv2/videoio/videoio.hpp"
+#include "opencv2/core.hpp"
+
+class video_reader
+{
+	private:
+		VideoCapture vcap;
+	public:
+};
+
+
+
+
+#endif /* SRC_READ_VIDEO_H_ */
